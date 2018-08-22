c4b28892d59b1d17b055b1b2c006afb8cc18cb50
pablo.mint <slpkvol3@gmail.com>
2018-08-22 02:24:12 -0300

    Tests for Calculator initialization

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,7 @@
 class Calculator
-  def new(db_instance)
+  def initialize(db_instance)
     raise ArgumentError.new("No DB instance provided") if db_instance.nil?
-    raise ArgumentError.new("DB instance is not ready") unless db_instance.is_ready?
+    raise ArgumentError.new("DB instance is not ready") unless db_instance.ready?
   end
 
   # distance in km
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,18 +1,38 @@
-require './calculator'
+require 'calculator'
 
-RSpec.describe "setting up integration" do
-  before :all do
+RSpec.describe Calculator do
+  before(:example) do
+    data = [
+      {
+        latitude: "52.986375",
+        user_id: 12,
+        name: "Christina McArdle",
+        longitude: "-6.043701"
+      },
+      {
+        latitude: "51.92893",
+        user_id: 1,
+        name: "Alice Cahill",
+        longitude: "-10.27699"
+      },
+      {
+        latitude: "51.8856167",
+        user_id: 2,
+        name: "Ian McArdle",
+        longitude: "-10.4240951"
+      }
+    ]
+    @db = double("mock db instance", get_all: data, ready?: false)
   end
-end
 
-RSpec.describe "integration" do
-
-  describe SimpleCals do
-    subject(:result) do
+  context "#new" do
+    it "checks for empty db instance" do
+      expect { Calculator.new }.to raise_error(ArgumentError)
+      expect { Calculator.new(nil) }.to raise_error(ArgumentError)
     end
 
-    it "just works" do
-      expect(true).to eq true
+    it "checks for db instance unreadyness" do
+      expect { Calculator.new(@db) }.to raise_error(ArgumentError)
     end
   end
 end
