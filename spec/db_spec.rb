e39186d5b3c65accfc5029f883d53ab40075540f
pablo.mint <slpkvol3@gmail.com>
2018-08-22 01:01:48 -0300

    Test for DB retrieve methods

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -29,4 +29,50 @@ RSpec.describe DB do
       File.delete('carlos.json')
     end
   end
+
+  context "#get" do
+    before(:example) do
+      @data = [
+        {
+          latitude: "52.986375",
+          user_id: 12,
+          name: "Christina McArdle",
+          longitude: "-6.043701"
+        },
+        {
+          latitude: "51.92893",
+          user_id: 1,
+          name: "Alice Cahill",
+          longitude: "-10.27699"
+        },
+        {
+          latitude: "51.8856167",
+          user_id: 2,
+          name: "Ian McArdle",
+          longitude: "-10.4240951"
+        }
+      ]
+      IO.write('customers.json', JSON.dump(@data))
+    end
+
+    it "can retrieve all data after loading it" do
+      db = DB.new('customers.json')
+      res = db.get_all
+      expect(res.length).to be equal(@data.length)
+      expect(res.first).to be equal(@data.first)
+      expect(res.second).to be equal(@data.second)
+      expect(res.third).to be equal(@data.third)
+    end
+
+    it "can retrieve part of the data after loading it" do
+      db = DB.new('customers.json')
+      res = db.get(1)
+      expect(res.length).to be equal(1)
+      expect(res.first).to be equal(@data.first)
+    end
+
+    after(:example) do
+      File.delete('customers.json')
+    end
+  end
 end
