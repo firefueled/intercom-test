beba250e6de240035facd64797176066413ac51a
pablo.mint <slpkvol3@gmail.com>
2018-08-22 01:32:11 -0300

    Test for actual input data format

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -3,6 +3,31 @@ require 'json'
 require 'db'
 
 RSpec.describe DB do
+  before(:example) do
+    @data = [
+      {
+        latitude: "52.986375",
+        user_id: 12,
+        name: "Christina McArdle",
+        longitude: "-6.043701"
+      },
+      {
+        latitude: "51.92893",
+        user_id: 1,
+        name: "Alice Cahill",
+        longitude: "-10.27699"
+      },
+      {
+        latitude: "51.8856167",
+        user_id: 2,
+        name: "Ian McArdle",
+        longitude: "-10.4240951"
+      }
+    ]
+    txt_data = @data.map { |x| JSON.dump(x) }
+    IO.write('customers.txt', txt_data.join("\n"))
+  end
+
   context "#new" do
     it "checks for blank file path" do
       expect { DB.new }.to raise_error(ArgumentError)
@@ -14,49 +39,20 @@ RSpec.describe DB do
       expect { DB.new('9876trfvbnmk.87tfvb') }.to raise_error(ArgumentError)
     end
 
-    it "checks for bad JSON data" do
+    it "checks for bad data format" do
       expect { DB.new('Gemfile') }.to raise_error(ArgumentError)
     end
 
     it "becomes ready when valid data is loaded" do
-      IO.write(
-        'carlos.json',
-        JSON.dump({ name: 'carlos', age: 3, aliases: [ 'hi', 'lo' ] })
-      )
-
-      db = DB.new('carlos.json')
+      db = DB.new('customers.txt')
       expect(db.ready?).to be true
-      File.delete('carlos.json')
+      File.delete('customers.txt')
     end
   end
 
   context "#get" do
-    before(:example) do
-      @data = [
-        {
-          latitude: "52.986375",
-          user_id: 12,
-          name: "Christina McArdle",
-          longitude: "-6.043701"
-        },
-        {
-          latitude: "51.92893",
-          user_id: 1,
-          name: "Alice Cahill",
-          longitude: "-10.27699"
-        },
-        {
-          latitude: "51.8856167",
-          user_id: 2,
-          name: "Ian McArdle",
-          longitude: "-10.4240951"
-        }
-      ]
-      IO.write('customers.json', JSON.dump(@data))
-    end
-
     it "can retrieve all data after loading it" do
-      db = DB.new('customers.json')
+      db = DB.new('customers.txt')
       res = db.get_all
       expect(res.length).to be equal(@data.length)
       expect(res.first).to be equal(@data.first)
@@ -65,14 +61,14 @@ RSpec.describe DB do
     end
 
     it "can retrieve part of the data after loading it" do
-      db = DB.new('customers.json')
+      db = DB.new('customers.txt')
       res = db.get(1)
       expect(res.length).to be equal(1)
       expect(res.first).to be equal(@data.first)
     end
 
     after(:example) do
-      File.delete('customers.json')
+      File.delete('customers.txt')
     end
   end
 end
