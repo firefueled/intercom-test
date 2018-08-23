f64a712f130a1ed5646d48231b9b987c96759b6d
pablo.mint <slpkvol3@gmail.com>
2018-08-23 00:00:09 -0300

    Add test for first calculations. don't know the results yet...

--- spec/simple_calcs_integration_spec.rb
+++ spec/simple_calcs_integration_spec.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 RSpec.describe "SimpleCals" do
   context "#command line" do
     it "doesn't crash and burn" do
@@ -12,6 +14,21 @@ RSpec.describe "SimpleCals" do
       res = `./simple_calcs.rb`
       expect($?).to eq(0)
       expect(res.start_with?('Usage: simple_calcs.rb')).to be true
+    end
+  end
+
+  context "#calculation" do
+    before(:example) do
+      @correct_results = JSON.parse(IO.read('./spec/customers_result.txt'), symbolize_keys: true)
+      @correct_results.sort! { |a, b| a[:user_id] <=> b[:user_id] }
+    end
+
+    it "returns a result when given input as a file path" do
+      res = `./simple_calcs.rb -f customers.txt`
+      res_length = res.split("\n").length
+      # the expected result is one line per customer plus one description line
+      expect(res_length).to be >= @correct_results.length
+      expect(res_length).to be < @correct_results.length + 1
     end
   end
 end
