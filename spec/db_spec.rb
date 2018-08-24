92fa01ce029776186a2f297fe67d8a75204ad292
pablo.mint <slpkvol3@gmail.com>
2018-08-23 22:38:34 -0300

    Test for white space on input

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -5,27 +5,12 @@ require 'db'
 RSpec.describe DB do
   before(:example) do
     @data = [
-      {
-        latitude: "52.986375",
-        user_id: 12,
-        name: "Christina McArdle",
-        longitude: "-6.043701"
-      },
-      {
-        latitude: "51.92893",
-        user_id: 1,
-        name: "Alice Cahill",
-        longitude: "-10.27699"
-      },
-      {
-        latitude: "51.8856167",
-        user_id: 2,
-        name: "Ian McArdle",
-        longitude: "-10.4240951"
-      }
+      {latitude: "52.986375", user_id: 12, name: "Christina McArdle", longitude: "-6.043701"},
+      {latitude: "51.92893", user_id: 1, name: "Alice Cahill", longitude: "-10.27699"},
+      {latitude: "51.8856167", user_id: 2, name: "Ian McArdle", longitude: "-10.4240951"}
     ]
     txt_data = @data.map { |x| JSON.dump(x) }
-    IO.write('customers.txt', txt_data.join("\n"))
+    IO.write('customers_test.txt', txt_data.join("\n"))
   end
 
   context "#new" do
@@ -44,15 +29,14 @@ RSpec.describe DB do
     end
 
     it "becomes ready when valid data is loaded" do
-      db = DB.new('customers.txt')
+      db = DB.new('customers_test.txt')
       expect(db.ready?).to be true
-      File.delete('customers.txt')
     end
   end
 
   context "#get" do
     it "can retrieve all data" do
-      db = DB.new('customers.txt')
+      db = DB.new('customers_test.txt')
       res = db.get_all
       expect(res.length).to eq(@data.length)
       expect(res[0]).to eq(@data[0])
@@ -61,14 +45,45 @@ RSpec.describe DB do
     end
 
     it "can retrieve part of the data" do
-      db = DB.new('customers.txt')
+      db = DB.new('customers_test.txt')
       res = db.get(1)
       expect(res.length).to eq(1)
       expect(res[0]).to eq(@data[0])
     end
+  end
 
-    after(:example) do
-      File.delete('customers.txt')
+  context "#handle problematic input" do
+    it "ignores white spaces" do
+      data = [
+        {latitude: "52.986375", user_id: 12, name: "Christina McArdle", longitude: "-6.043701"},
+        {latitude: "51.92893", user_id: 1, name: "Alice Cahill", longitude: "-10.27699"},
+        {latitude: "51.8856167", user_id: 2, name: "Ian McArdle", longitude: "-10.4240951"}
+      ]
+      data.map! { |x| JSON.dump(x) }
+
+      File.open('input_test.txt', 'w') do |file|
+        file.write("\t      \n")
+
+        file.write("#{data[0]}\n")
+        file.write("#{data[1]}    \n")
+        file.write("    #{data[2]}\n")
+
+        file.write("   \t\n")
+      end
+
+      db = DB.new('input_test.txt')
+      res = db.get_all
+
+      expect(res.length).to be 3
+      expect(res[0][:user_id]).to eq data[0][:user_id]
+      expect(res[1][:user_id]).to eq data[1][:user_id]
+      expect(res[2][:user_id]).to eq data[2][:user_id]
+
+      File.delete('input_test.txt')
     end
+  end
+
+  after(:example) do
+    File.delete('customers_test.txt')
   end
 end
